c6dd6704c51f5acb89ca407f3e6650bba158345e
zunerz671 <zunerz671@gmail.com>
2025-11-21 19:20:46 +1000

    deployment ready.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:3000'
+const API_BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://v1mini-capstone-api.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
